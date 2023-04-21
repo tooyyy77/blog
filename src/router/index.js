e533007c9e248f3945afe4a85959c99d7ea937e4
tooyyy77 <1002531807@qq.com>
2023-04-21 11:57:19 +0800

    更新了footer的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import sidebar from "@components/sidebar.vue";
+import footer from "@components/footer.vue"
 import articleList from "@views/articleList.vue";
 import articleListByTag from "@views/articleListByTag.vue";
 import articleListByTitle from "@views/articleListByTitle.vue";
 import article from "@views/article.vue";
 
 const routes = [
-  { path: "/", components: { default: articleList, sidebar: sidebar } },
+  { path: "/", components: { default: articleList, sidebar: sidebar ,footer: footer} },
   { path: "/searchByTag/:tag", name:"searchByTag",components: { default: articleListByTag, sidebar: sidebar } },
   { path: "/searchByTitle/:title", name:"searchByTitle",components: { default: articleListByTitle, sidebar: sidebar } },
   { path: "/article/:id",name:"article", component: article },
